9c8fd00a9f60c7f14268c2a3d94e831bc870c5af
Amir Liaqat <you@example.com>
2022-07-19 16:07:24 -0700

    Creating new file for enqueue styles and scripts..

---
+++ inc/Enqueue.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package BBSmtp
+ */
+
+class Enqueue {
+
+    public static function enqueue() {
+        wp_enqueue_style( 'bb-smtp-style', PLUGIN_URL . 'assets/css/style.css' );
+        wp_enqueue_script( 'bb-smtp-script', PLUGIN_URL . 'assets/js/script.js' );
+    }
+    
+}
