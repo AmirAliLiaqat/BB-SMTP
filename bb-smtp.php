603450952d0196828191c6acc96dd662eddaf1c4
Amir Liaqat <you@example.com>
2022-07-19 15:54:19 -0700

    Define constants for easy to use...

--- bb-smtp.php
+++ bb-smtp.php
@@ -8,3 +8,23 @@
  * Description: BB Smpt plugin is basically designed for sending mails from localhost to gmail through smtp.gmail.com
  * License:     GPL v2 or Later
 */
+
+// Define condition to stop the direct access of file.
+if( !defined( 'ABSPATH' ) ) {
+    die();
+}
+
+// Define constant for plugin url
+if( !defined( 'PLUGIN_PATH' ) ) {
+    define( 'PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+}
+
+// Define constant for plugin path
+if( !defined( 'PLUGIN_URL' ) ) {
+    define( 'PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+}
+
+// Define constant for plugin basename
+if( !defined( 'PLUGIN' ) ) {
+    define( 'PLUGIN', plugin_basename( __FILE__ ) );
+}
