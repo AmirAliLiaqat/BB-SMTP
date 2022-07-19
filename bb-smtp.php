e3a68bbb16f3230be2840ecc564898d2ab2a91d9
Amir Liaqat <you@example.com>
2022-07-19 15:52:50 -0700

    Adding plugin identity comments

---
+++ bb-smtp.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: BB SMTP
+ * Plugin URI:  https://amir.bytebunch.com/plugins
+ * Author:      Byte Ki Duniya
+ * Author URI:  https://amir.bytebunch.com
+ * Version:     1.0.0
+ * Description: BB Smpt plugin is basically designed for sending mails from localhost to gmail through smtp.gmail.com
+ * License:     GPL v2 or Later
+*/
