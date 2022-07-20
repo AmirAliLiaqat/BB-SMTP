a9a74690552419aa419e95633e445695569649b5
Amir Liaqat <you@example.com>
2022-07-20 08:24:55 -0700

    Adding settings and fields code...

--- templates/admin.php
+++ templates/admin.php
@@ -3,8 +3,12 @@
     <h1 class="main-heading">BB SMTP</h1>
     <?php settings_errors(); ?>
     <div class="wrap-content">
-        <form action="" method="post">
-            <?php submit_button(); ?>
+        <form action="options.php" method="post">
+            <?php 
+                settings_fields( 'bb_smtp_main_settings' );
+                do_settings_sections( 'bb_smtp_main_settings' );
+                submit_button();
+            ?>
         </form>
     </div><!--wrap-content-->
 </div>
