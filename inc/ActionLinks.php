a8a2d7c1196a55a9b481825644b1ee8620adf1aa
Amir Liaqat <you@example.com>
2022-07-19 20:27:43 -0700

    Update setting link url...

--- inc/ActionLinks.php
+++ inc/ActionLinks.php
@@ -6,7 +6,7 @@
 class ActionLinks {
 
     public static function action_links( $links ) {
-        $settings_link = '<a href="admin.php?bb_smtp">Settings</a>';
+        $settings_link = '<a href="admin.php?page=bb_smtp">Settings</a>';
         array_push( $links, $settings_link );
         return $links;
     }
