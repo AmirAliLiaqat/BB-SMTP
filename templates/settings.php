694a0cde763aff1746090f17337e0450dbbf965e
Amir Liaqat <you@example.com>
2022-07-23 11:59:15 +0500

    Creating new file for plugin settings page html...

---
+++ templates/settings.php
@@ -0,0 +1,25 @@
+<div class="warp">
+    <h1 class="main-heading">BB SMTP</h1>
+    <div class="wrap-content">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Message</th>
+                    <th>Published</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Alex</td>
+                    <td>alex@gmail.com</td>
+                    <td>Hello, How are you?</td>
+                    <td>22/07/2022</td>
+                </tr>
+            </tbody>
+        </table>
+    </div><!--wrap-content-->
+</div><!--wrap-->
